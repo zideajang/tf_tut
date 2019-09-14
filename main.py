8538a5a15e8b67458833b609bdcd69693312eff2
jangwoo <zidea2015@163.com>
2019-09-14 14:55:26 +0800

    创建 cifarData 管理类

--- main.py
+++ main.py
@@ -3,32 +3,56 @@ import tensorflow as tf
 import numpy as np
 import pickle
 import os
+import utils
+from utils import CifarData
 # save file format as numpy
 # file format as cPickle
 CIFAR_DIR = "./cifar-10-batches-py"
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 # print(os.listdir(CIFAR_DIR))
 
+train_filenames = [os.path.join(CIFAR_DIR, 'data_batch_%d' % i)
+                   for i in range(1, 6)]
+test_filenames = [os.path.join(CIFAR_DIR, 'test_batch')]
+train_data = CifarData(train_filenames, True)
+test_data = CifarData(test_filenames, False)
+
 # ['data_batch_1', 'readme.html', 'batches.meta', 'data_batch_2', 'data_batch_5', 'test_batch', 'data_batch_4', 'data_batch_3']
 
-with open(os.path.join(CIFAR_DIR,"data_batch_1"),'rb') as f:
-    data = pickle.load(f)
-    print(type(data))
-    print(data.keys())
-    print(type(data['data']))
-    print(type(data['labels']))
-    print(type(data['batch_label']))
-    print(type(data['filenames']))
-    print(data['data'].shape)
-    print(data['data'][0:2])
-    print(data['labels'][0:2])
-    print(data['batch_label'])
-    print(data['filenames'][0:2])
+# [None]
+# define input and output placeholder recieved input 
+x = tf.placeholder(tf.float32,[None,3072])
+y = tf.placeholder(tf.int64,[None])
+# (3071,1)
+w = tf.get_variable('w',[x.get_shape()[-1],1],initializer=tf.random_normal_initializer(0,1))
+# (1,)
+b = tf.get_variable('b',[1],initializer=tf.constant(0.0))
 
-image_arr = data['data'][100]
-image_arr = image_arr.reshape((3,32,32))
-image_arr = image_arr.transpose((1,2,0))
+# [None,3072] * [3072,1] = [None,1]
+# only y_ f(x) = x * W + b
+y_ = tf.matmul(x,w) + b
 
-import matplotlib.pyplot as plt
-# plt.imshow(image_arr)
-# plt.show()
+# [None,1]
+# change f(x) output in (0,1) range
+p_y_1 = tf.nn.sigmoid(y_)
+
+# 
+y_reshaped = tf.reshape(y,(-1,1))
+y_reshaped_float = tf.cast(y_reshaped,tf.float32)
+
+# avg(y_prep**2 - y_expect**2) loss = L(f(x) - y)
+loss = tf.reduce_mean(tf.square(y_reshaped_float - p_y_1))
+
+# bool
+predict = p_y_1 > 0.5
+correct_prediction = tf.equal(tf.cast(predict,tf.int64),y_reshaped)
+
+# [1,0,0,1,1,]
+accuracy = tf.reduce_mean(tf.cast(correct_prediction,tf.float64))
+
+with tf.name_scope('train_op'):
+    # learning rate le-3 
+    train_op = tf.train.AdamOptimizer(le-3).minimize(loss)
+init = tf.global_variables_initializer()
+with tf.Session() as sess:
+    sess.run([loss,accuracy,train_op],feed_dict={x:,y: })
