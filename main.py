94c1ef1e8a8f5ec49c604f16937f899410b84c06
jangwoo <zidea2015@163.com>
2019-09-14 13:02:49 +0800

    第一次提交

---
+++ main.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+# print(tf.__version__)
+import numpy as np
+import cPickle
+# save file format as numpy
+# file format as cPickle
+CIFAR_DIR = "./cifar-10-batches-py"
+
+print(os.listdir(CIFAR_DIR))
