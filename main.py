8af099a9587e042c424234cd5eb32e6723ccded9
jangwoo <zidea2015@163.com>
2019-09-14 13:04:54 +0800

    打印cifar目录内文件内容

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ import tensorflow as tf
 # print(tf.__version__)
 import numpy as np
 import cPickle
+import os
 # save file format as numpy
 # file format as cPickle
 CIFAR_DIR = "./cifar-10-batches-py"
 
 print(os.listdir(CIFAR_DIR))
+
+# ['data_batch_1', 'readme.html', 'batches.meta', 'data_batch_2', 'data_batch_5', 'test_batch', 'data_batch_4', 'data_batch_3']
